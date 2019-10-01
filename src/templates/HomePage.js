840ea72f1b8cded9dbb40553bbb03602ff4c6bab
Taco <tacodntr@gmail.com>
2019-10-01 17:57:16 +0800

    bunch of style changes

--- src/templates/HomePage.js
+++ src/templates/HomePage.js
@@ -16,7 +16,7 @@ export const HomePageTemplate = ({ title, subtitle, featuredImage, introduction 
     />
 
     <section className="section">
-      <div className="container skinnier">
+      <div className="container skinny">
         <Content source={introduction} />
         <a class="Button" href="/blog">View case studies</a>
       </div>
